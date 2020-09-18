c3cd2e59842939f701237f24fab839b3c36d0e3d
Jacob Schnoor <schno059@d.umn.edu>
2020-09-17 20:38:12 -0500

    imported profanity check

--- Eliza/__init__.py
+++ Eliza/__init__.py
@@ -1,18 +1,38 @@
-# coding=utf8
-# the above tag defines encoding for this document and is for Python 2.x compatibility
-
 import re
+from random import *
+from profanity_check import predict, predict_prob
 
-regex = r"(?:(?:I|you|he|she|they|we|it)(?:.*)\s(?:know|think|feel|seem)s?(?:\sthat\s)?(?:\slike\s)?)?(.*)\bme\b([^!\.?\n]*)(.*)$"
+name = ''
+my_rand_int = randint(0, 19)
 
-test_str = input('Why did you come here today?\n'.upper())
 
-subst = "Why do you feel like \\1you\\2?"
+# input:        string that contains a user's first name
+# output:       the user's first name
+# description:  scans the sentence for instances such as "my name is..." or "i am..." then
+#               grabs the following word. otherwise it will just grab the first word
+def name_extractor(test_str):
+    regex = r"((.*\bmy\sname\sis\s)|(.*\bi\sam\s)|(.*\bi'?m\s)|(^\s))?(\S+)\b(.*)"
+    subst = "\\6"
+    result = re.sub(regex, subst, test_str, 0, re.MULTILINE | re.IGNORECASE).capitalize()
+    return result
 
-# You can manually specify the number of replacements by changing the 4th argument
-result = re.sub(regex, subst, test_str, 0, re.MULTILINE | re.IGNORECASE)
 
-if result:
-    print(result.upper())
+def introduction():
+    print("This is Eliza the Academic Advisor, made by Jacob Schnoor\n")
+    name = name_extractor(input("[eliza] Hi I'm Eliza. Remind me what your name is again?\n\n"))
+    print('\nHi ' + name + '. What is your major?')
 
-# Note: for Python 2.7 compatibility, use ur"" to prefix the regex and u"" to prefix the test string and substitution.
+
+# regex = r"(?:(?:I|you|he|she|they|we|it)(?:.*)\s(?:know|think|feel|seem)s?(?:\sthat\s)?(?:\slike\s)?)?(.*)\bme\b([^!\.?\n]*)(.*)$"
+#
+# test_str = input('Why did you come here today?\n'.upper())
+#
+# subst = "Why do you feel like \\1you\\2?"
+#
+# result = re.sub(regex, subst, test_str, 0, re.MULTILINE | re.IGNORECASE)
+#
+# if result:
+#     print(result.upper())
+
+# Main
+introduction()
