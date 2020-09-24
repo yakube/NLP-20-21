6533ab75c96572e7f88c451686ac400d32704fc8
Jacob Schnoor <schno059@d.umn.edu>
2020-09-23 19:36:40 -0500

    Fixed gibberish detection. Mainly just need some comments in the code, a few more questions to the user, and an interjection proving Eliza remembered something

--- Eliza/__init__.py
+++ Eliza/__init__.py
@@ -227,10 +227,11 @@ def topic_change():
 def is_swearing(user_in):
     return profanity.contains_profanity(user_in)
 
-#---------Find better regex-----------
+
 def is_gibberish(user_in):
-    regex = r"(((.*?)(\b[^aeiouy\s\n]+\s)+(.*))|((.*?)(a{3,}|b{3,}|c{3,}|d{3,}|e{3,}|f{3,}|g{3,}|h{3,}|i{3,}|j{3," \
-            r"}|k{3,}|l{3,}|m{3,}|n{3,}|o{3,}|p{3,}|q{3,}|r{3,}|s{3,}|t{3,}|u{3,}|v{3,}|w{3,}|x{3,}|y{3,}|z{3,})(.*)))"
+    regex = r"(((.*?)((^|[\n\t ])[^aeiouy\s\n0-9]+($|[\n\t ]))+(.*))|((.*?)(a{3,}|b{3,}|c{3,}|d{3,}|e{3,}|f{3,}|g{3," \
+            r"}|h{3,}|i{3,}|j{3,}|k{3,}|l{3,}|m{3,}|n{3,}|o{3,}|p{3,}|q{3,}|r{3,}|s{3,}|t{3,}|u{3,}|v{3,}|w{3,}|x{3," \
+            r"}|y{3,}|z{3,})(.*)))"
     if re.match(regex, user_in, re.MULTILINE | re.IGNORECASE):
         return True
     else:
@@ -382,7 +383,7 @@ def main_script():
     print("This is Eliza the Academic Advisor, made by Jacob Schnoor")
     # print(pronoun_flip(input("Pronoun Flip: ")))
     name = extract_name(scan(ask_name(), True))
-    major = extract_major(scan(ask_major(), False))
+    major = extract_major(scan(ask_major(), True))
     scan(ask_feelings(), True)
     scan(ask_hometown(), False)
     scan(ask_gradyear(), False)
