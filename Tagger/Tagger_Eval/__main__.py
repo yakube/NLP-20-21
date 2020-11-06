7be3f7a04707de9d13c02b76e97f7819fee9ba7a
Jacob Schnoor <schno059@d.umn.edu>
2020-11-06 14:00:56 -0600

    working on PA5 trainer

--- Tagger/Tagger_Eval/__main__.py
+++ Tagger/Tagger_Eval/__main__.py
@@ -1,1 +1,23 @@
 # Evaluator Program
+import glob
+import os
+import re
+import sys
+from collections import Counter
+from itertools import islice
+from math import sqrt, log
+
+
+def main(argv):
+    input_filename = argv[0]
+    output_filename = argv[1]
+
+    print("\nPOS Tagger Evaluator. CS4242. Made by Jacob Schnoor\n")
+    print("Input File:    \t" + input_filename)
+    print("Output File: \t" + output_filename + "\n")
+
+    f = open(input_filename, encoding="utf8")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- Tagger/Tagger_Train/__main__.py
+++ Tagger/Tagger_Train/__main__.py
@@ -1,1 +1,38 @@
 # Trainer Program
+import re
+import sys
+from collections import Counter
+from itertools import islice
+
+word_tag_pairs = Counter([])
+word_counter = Counter([])
+word_tag_probs = dict()
+
+
+def main(argv):
+    input_filename = argv[0]
+    output_filename = argv[1]
+
+    print("\nPOS Tagger Trainer. CS4242. Made by Jacob Schnoor\n")
+    print("Training data:    \t" + input_filename)
+    print("Probability file: \t" + output_filename + "\n")
+
+    r_split = re.split(r"(.*)(?<!\\)\/(.*)", open(input_filename, encoding="utf8").read())
+
+    word_counter.update(islice(r_split, 1, None, 3))
+    word_tag_pairs.update(zip(islice(r_split, 1, None, 3),
+                              islice(r_split, 2, None, 3)))
+
+    # Need to make probability dictionary----------------------------------------------------------------......???
+    # for x in word_tag_pairs:
+
+    # out = open(output_filename, "w")
+    # for word, word_count in word_counter.most_common():
+    #     for (pair_word, tag), pair_count in word_tag_pairs.most_common():
+    #         if pair_word == word:
+    #             out.write("{:15s}\t{:5s}\t{:8f}\n".format(word, tag, pair_count / word_count))
+    # out.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
