c5c7f3fe1ee93d92409fdc9d2b93d9af7316d86e
Jacob Schnoor <schno059@d.umn.edu>
2020-11-12 10:29:08 -0600

    NNP checker

--- Tagger/Tagger_Test/__main__.py
+++ Tagger/Tagger_Test/__main__.py
@@ -8,35 +8,42 @@ from itertools import islice
 from math import sqrt, log
 
 
+def find_something(word):
+    print(word)
+    print(word[:1])
+    if word[:1].isupper():
+        return "NNP"
+    return "NN"
+
+
 def main(argv):
-    mode = argv[0]
+    mode = int(argv[0])
     probability_listname = argv[1]
     test_filename = argv[2]
     output_filename = argv[3]
 
     print("\nPOS Tagger Tester. CS4242. Made by Jacob Schnoor\n")
-    print("Mode:            \t" + mode)
+    print("Mode:            \t" + str(mode))
     print("Probability List:\t" + probability_listname)
     print("Test File:       \t" + test_filename)
     print("Output File:     \t" + output_filename + "\n")
 
     test_file = open(test_filename, encoding="utf8")
     out = open(output_filename, "w")
-    if mode == str(0):
-        for line in test_file:
-            probability_list = open(probability_listname, encoding="utf8")
-            tag = "NN"
-            has_found = False
+    for line in test_file:
+        probability_list = open(probability_listname, encoding="utf8")
+        tag = "NN"
+        has_found = False
+        next_line = probability_list.readline()
+        while not has_found and next_line != "":
+            prob_line_split = next_line.split()
+            if prob_line_split[0] == line[:-1]:
+                has_found = True
+                tag = prob_line_split[1]
             next_line = probability_list.readline()
-            while not has_found and next_line != "":
-                prob_line_split = next_line.split()
-                if prob_line_split[0] == line[:-1]:
-                    has_found = True
-                    tag = prob_line_split[1]
-                next_line = probability_list.readline()
-            out.write(line[:-1] + "/" + tag + "\n")
-    else:
-        print("Mode 1 in progress...")
+        if not has_found and mode == 1:
+            tag = find_something(line[:-1])
+        out.write(line[:-1] + "/" + tag + "\n")
     print("DONE")
 
 
