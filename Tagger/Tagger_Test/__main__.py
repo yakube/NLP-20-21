7bd63694aa7b335377e1a268056969d82b270751
Jacob Schnoor <schno059@d.umn.edu>
2020-11-17 10:51:34 -0600

    partially commented

--- Tagger/Tagger_Test/__main__.py
+++ Tagger/Tagger_Test/__main__.py
@@ -1,9 +1,74 @@
+# --------------------------------
+# Tagger Tester
+# Made by Jacob Schnoor
+# 11/17/20
+#
+# ==What is it?
+#
+# The Tagger Tester is the second of a three part program to tag parts of speech. For more information on the general
+# program, see the Tagger Trainer.
+#
+#
+# The Tagger Tester specifically serves the role of assigning tags to a test set of unlabeled words. It does this by
+# referencing the probability list and comparing those words to the words in the test set. Anything that is not found
+# in the probability list requires the program to more or less just guess the tag. By assigning tags to an unlabeled
+# set, we can later compare the program's responses to an answer key and get an idea of its accuracy.
+#
+# ==How do I use it?
+#
+# The Tagger Tester runs in Python off the commandline with the following format:
+#
+# 	python Tagger_Test mode probability_list.txt test.txt test-mode.txt
+#
+# Where mode is either 0 or 1 (mode 0 is baseline, mode 1 is more accurate). probability_list.txt is a well
+# formatted, sorted list of words, tags, and their respective likelihoods. test.txt is the test set where every line
+# is a single word. test-mode.txt is the name of the output file that will later be graded by Tagger Evaluator.
+#
+# For example, one may type:
+#
+# 	python Tagger_Test 0 tagger-train-prob.txt pos-test.txt pos-test-0.txt
+#
+# And get the following output in pos-test-0.txt:
+#
+# No/DT
+# ,/,
+# it/PRP
+# was/VBD
+# n't/RB
+# Black/NNP
+# Monday/NNP
+# ./.
+# But/CC
+# while/IN
+#    .
+#    .
+#    .
+# in/IN
+# Europe/NNP
+# Friday/NNP
+# ./.
+#
+# (File is about 57,000 lines long)
+#
+# ==How does it work?
+#
+# The Tagger Tester is broken up into a few pieces (more details follow in the code):
+#
+# 	1) Initialize variables, introduce user, and open files
+# 	2) Set the word tag equal to the most likely tag for that word from the probability list, if possible
+# 	3) [Mode 0]: If not possible, just call it a noun
+# 	4) [Mode 1]: If not possible, correct using A-rules
+# 	5) [Mode 1]: If the word was found, double check it with B-rules
+# 	6) Write the words with their tags appended on in the output file
+# 	7) Indicate completion
+
 import sys
 
 
-# A - Rules
+# 4) A - Rules for guessing at unseen words in Mode 1. Rules are explained below
 def correct_unknown(word, previous_tag):
-    # A-1
+    # A-1 : If the unseen word is preceded by a token of type "TO" it is determined to be a verb
+    # Example: "I like TO RUN" guesses that "RUN" is a verb
     if previous_tag == "TO":
         return "VB"
     if word[:1].isupper():
@@ -52,23 +117,30 @@ def correct_known(word, current_tag, previous_tag):
 
 
 def main(argv):
+    # 1) Splits user arguments into all the specified variables
     mode = int(argv[0])
     probability_listname = argv[1]
     test_filename = argv[2]
     output_filename = argv[3]
 
+    # 1) Introduces the user
     print("\nPOS Tagger Tester. CS4242. Made by Jacob Schnoor\n")
     print("Mode:            \t" + str(mode))
     print("Probability List:\t" + probability_listname)
     print("Test File:       \t" + test_filename)
     print("Output File:     \t" + output_filename + "\n")
 
+    # 1) Opens files and sets a default tag of "NN" for singular noun
     test_file = open(test_filename, encoding="utf8")
     out = open(output_filename, "w")
     tag = "NN"
+    # 2) Goes line by line in the test_file, checks if that word is anywhere to be found in the probability list.
+    # If so, it assigns the word the most likely tag
     for line in test_file:
         probability_list = open(probability_listname, encoding="utf8")
         previous_tag = tag
+        # 3) Tag is initially set to "NN" until it's changed by something later on. If it's never changed, then it just
+        # stays as a noun
         tag = "NN"
         has_found = False
         next_line = probability_list.readline()
@@ -78,12 +150,17 @@ def main(argv):
                 has_found = True
                 tag = prob_line_split[1]
             next_line = probability_list.readline()
+        # Mode 1 for improved accuracy
         if mode == 1:
+            # 5) If a word was found in the probability list, double check its veracity with B-rules
             if has_found:
                 tag = correct_known(line[:-1], tag, previous_tag)
+            # 4) If nothing was found in the original probability list, guess the type using A-rules
             else:
                 tag = correct_unknown(line[:-1], previous_tag)
+        # 6) Writes the word tag pair to the output file
         out.write(line[:-1] + "/" + tag + "\n")
+    # 7) Indicates completion
     print("DONE")
 
 
--- Tagger/Tagger_Train/__main__.py
+++ Tagger/Tagger_Train/__main__.py
@@ -1,8 +1,73 @@
+# --------------------------------
+# Tagger Trainer
+# Made by Jacob Schnoor
+# 11/17/20
+#
+# ==What is it?
+#
+# The Tagger Trainer is the first of a three part program that seeks to do part-of-speech tagging on strings of text.
+# This means that ideally, the Tagger programs will be able to look at a previously unseen sentence, and identify
+# which words are nouns, which are verbs, which are adjectives, etc. This is useful for a wide variety of
+# applications such as identifying named entities.
+#
+# The Trainer specifically serves the role of scanning a corpus of labeled text. By looking at over a million tokens
+# in context, with their correct tags, a probability list can be generated that will be used later on.
+#
+# ==How do I use it?
+#
+# The Tagger Trainer is a Python program that runs off the commandline with the following format:
+#
+# 	python Tagger_Train training_data.txt probability_list.txt
+#
+# Where training_data.txt is an appropriately formatted .txt file with every line being a word and its respective
+# tag. probability_list.txt will be the output file with a list of all unique tokens, their tags, and the likelihood
+# of that tag. The list is sorted by likelihood.
+#
+# As an example. One might type:
+#
+# 	python Tagger_Train pos-train.txt tagger-train-prob.txt
+#
+# And get the following output in tagger-train-prob.txt
+#
+# ,                             	,    	0.999305
+# .                             	.    	0.999128
+# the                           	DT   	0.998869
+# of                            	IN   	0.998383
+# to                            	TO   	0.998367
+# and                           	CC   	0.997449
+# a                             	DT   	0.997096
+# $                             	$    	0.995225
+# ``                            	``   	0.995216
+# ''                            	''   	0.995209
+# 		.
+# 		.
+# 		.
+# a                             	IN   	0.000081
+# to                            	JJ   	0.000069
+# the                           	CD   	0.000037
+# the                           	NN   	0.000037
+# the                           	VBP  	0.000037
+#
+# (The list has about 59,000 lines)
+#
+# ==How does it work?
+#
+# The Tagger Trainer is broken down into a few main parts (more details can be found in the code):
+#
+# 	1) Initialize variables, open files, and introduce user
+# 	2) Split entire input file into array based on slashes and newline characters
+# 	3) Update word_counter, tag_counter, and word_tag_pairs with all the words and their respective tags
+# 	4) Create sorted dictionary of word-tag pair likelihoods
+# 	5) Write this list in a readable way to the output file
+# 	6) Indicate completion
+
 import re
 import sys
 from collections import Counter
 from itertools import islice
 
+# 1) This establishes the Counter objects and the dictionary object. Counters are high performance containers and are
+# very useful for counting up large amounts of unique token types. The dictionary makes for easy sorting later on
 word_tag_pairs = Counter([])
 word_counter = Counter([])
 tag_counter = Counter([])
@@ -10,29 +75,51 @@ word_tag_probs = dict()
 
 
 def main(argv):
+    # 1) Splits user arguments into the input filename and the output filename
     input_filename = argv[0]
     output_filename = argv[1]
 
+    # 1) Introduces the user and gives brief information
     print("\nPOS Tagger Trainer. CS4242. Made by Jacob Schnoor\n")
     print("Training data:    \t" + input_filename)
     print("Probability file: \t" + output_filename + "\n")
 
+    # 2) Reads the whole input file at once and breaks it apart using re.split(). This results in about half the list
+    # being blank or newline characters. But luckily, since the file is well formatted, every four elements are of the
+    # same type.
+    # e.g. list might be = ["","The","DT","\n","","seven","CD","\n",...etc.]
     r_split = re.split(r"(.*)(?<!\\)\/(.*)", open(input_filename, encoding="utf8").read())
 
+    # 3) This section updates all the relevant counters based on the list we just obtained. By using islice(),
+    # we essentially say, "Start at element x, end at element y, and increment by z". Since our list has a certain
+    # regularity to it, it becomes trivial to grab all words, all tags, and all word-tag pairs. The Counter objects
+    # can efficiently count them up and deal with duplicates. We will deal with these raw counts to determine our
+    # probabilities.
     word_counter.update(islice(r_split, 1, None, 3))
     tag_counter.update(islice(r_split, 2, None, 3))
     word_tag_pairs.update(zip(islice(r_split, 1, None, 3),
                               islice(r_split, 2, None, 3)))
 
+    # 4) We open the output file for writing. Then we make our dictionary of probabilities based on raw counts and
+    # LaPlace smoothing. For every word-tag pair that we find in our Counter object, we calculate probability by
+    # taking the occurrences of the word and tag together plus one, then divide by how many times that word occurs plus
+    # how many total tags there are.
+
+    # The reason we use LaPlace smoothing is that probability ordering is normally skewed toward rare events. For
+    # example, if a word only occurs once, then that word-tag pair would have a 100% chance of being the case, whereas
+    # something with 499/500 occurrences would be ranked lower on the list. I figured that a LaPlace smoothed list
+    # would be in a more useful order.
     out = open(output_filename, "w")
     for word, tag in word_tag_pairs:
         word_tag_probs[(word, tag)] = (word_tag_pairs[(word, tag)] + 1) / (word_counter[word] + len(tag_counter))
 
-    # for x in sorted(word_tag_probs.items(), key=lambda y: y[1], reverse=True):
+    # 5) We sort the list first based on probability, then alphabetically if the probabilities are equal.
+    # For every element in this list, we print out the word, the tag, and the probability. This gets written to the
+    # output file.
     for x in sorted(word_tag_probs, key=lambda y: (-word_tag_probs[y], y)):
         out.write("{:30s}\t{:5s}\t{:8f}\n".format(x[0], x[1], word_tag_probs[x]))
-        # print(x)
     out.close()
+    # 6) "DONE" is printed off to indicate that the process is complete.
     print("DONE")
 
 
